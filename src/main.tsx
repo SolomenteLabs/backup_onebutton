4991c8fbe51a92cc055356619fe9d730d1448cce
SolomenteLabs <scrt13@protonmail.com>
2025-07-20 03:11:32 -0600

    🐛 Fix: remove invalid import and use wallets instead

--- src/main.tsx
+++ src/main.tsx
@@ -2,50 +2,59 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChainProvider } from "@cosmos-kit/react";
-import { keplrExtensionWallet } from "@cosmos-kit/keplr";
+import { wallets } from "@cosmos-kit/keplr";
 
-const chains = [
+const coreumTestnet = [
   {
-    chain_name: "coreum-testnet",
+    chain_name: "coreum",
     chain_id: "coreum-testnet-1",
+    apis: {
+      rpc: [
+        {
+          address: "https://full-node.testnet-1.coreum.dev:26657",
+        },
+      ],
+      rest: [
+        {
+          address: "https://full-node.testnet-1.coreum.dev:1317",
+        },
+      ],
+    },
     pretty_name: "Coreum Testnet",
     bech32_prefix: "core",
-    apis: {
-      rpc: [{ address: "https://full-node.testnet-1.coreum.dev:26657" }],
-      rest: [{ address: "https://full-node.testnet-1.coreum.dev:1317" }],
-    },
-    slip44: 118,
-    currencies: [
-      {
-        coinDenom: "TESTCORE",
-        coinMinimalDenom: "utestcore",
-        coinDecimals: 6,
-        coinGeckoId: "",
-      },
-    ],
-    feeCurrencies: [
-      {
-        coinDenom: "TESTCORE",
-        coinMinimalDenom: "utestcore",
-        coinDecimals: 6,
-        coinGeckoId: "",
-      },
-    ],
-    stakeCurrency: {
-      coinDenom: "TESTCORE",
-      coinMinimalDenom: "utestcore",
-      coinDecimals: 6,
-      coinGeckoId: "",
-    },
-    features: ["ibc-transfer", "wasm"],
   },
 ];
 
-const wallets = [keplrExtensionWallet()];
+async function suggestCoreumTestnet() {
+  if (window.keplr) {
+    await window.keplr.experimentalSuggestChain({
+      chainId: "coreum-testnet-1",
+      chainName: "Coreum Testnet",
+      rpc: "https://full-node.testnet-1.coreum.dev:26657",
+      rest: "https://full-node.testnet-1.coreum.dev:1317",
+      bip44: { coinType: 990 },
+      bech32Config: {
+        bech32PrefixAccAddr: "core",
+        bech32PrefixAccPub: "corepub",
+        bech32PrefixValAddr: "corevaloper",
+        bech32PrefixValPub: "corevaloperpub",
+        bech32PrefixConsAddr: "corevalcons",
+        bech32PrefixConsPub: "corevalconspub",
+      },
+      currencies: [{ coinDenom: "CORE", coinMinimalDenom: "ucore", decimals: 6 }],
+      feeCurrencies: [{ coinDenom: "CORE", coinMinimalDenom: "ucore", decimals: 6 }],
+      stakeCurrency: { coinDenom: "CORE", coinMinimalDenom: "ucore", decimals: 6 },
+      coinType: 990,
+      features: ["stargate", "ibc-transfer", "no-legacy-stdTx"],
+    });
+  }
+}
+
+suggestCoreumTestnet();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChainProvider chains={chains} assetLists={[]} wallets={wallets}>
+    <ChainProvider chains={coreumTestnet} assetLists={[]} wallets={wallets}>
       <App />
     </ChainProvider>
   </React.StrictMode>
